7253f1ea1461bf705aec25681de4b212fb499b8d
Keith Crooc <keith.crooc@gmail.com>
2021-02-08 21:04:59 -0500

    added html routing to server.js

--- server.js
+++ server.js
@@ -30,8 +30,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useUnifiedTopology: true
 })
 
+// Routes
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/exercise.html'))
+})
+
+app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/stats.html'))
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`App is running on: ${PORT}!`)
 })
+
 
