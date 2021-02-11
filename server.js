53ea3da7e3472558c462acfcd689f52a625b9f0a
Keith Crooc <keith.crooc@gmail.com>
2021-02-10 22:11:42 -0500

    created routes folder, fixed get request in Api

--- server.js
+++ server.js
@@ -31,18 +31,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 })
 
 // Routes
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
-
-app.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/exercise.html'))
-})
-
-app.get("/stats", (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/stats.html'))
-})
-
+require("./routes/api-routes")(app)
+require("./routes/html-routes")(app)
 
 
 app.listen(PORT, () => {
